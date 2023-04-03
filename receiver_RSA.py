83bf6ceb1493b3a1a44ff6a6e4855828415c8d9e
nanni00 <giovanni.malaguti00@gmail.com>
2023-04-03 18:17:02 +0200

    RSA files updated

--- receiver_RSA.py
+++ receiver_RSA.py
@@ -12,7 +12,23 @@ PORT = int(sys.argv[1])
 
 
 def decode(c, d, n):
+    c = int(c)                
     return utils.find_modulus(c, d, n)
+
+
+def get_public_key_and_secret_key():
+    p = 47
+    q = 71
+    n = p * q
+    phi_n = utils.euler_function(n)
+
+    # choose a number coprime with the Euler function's value of n
+    e = 79
+
+    # calculate the secret key
+    _, secret_key, _ = utils.my_gcd(e, phi_n)
+
+    return n, e, secret_key
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -21,33 +37,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr =  s.accept()
 
     with conn:
-        print('connection from ', addr)
-        # choose two great prime numbers 
-        p = 47
-        q = 71
-        n = p * q
-        phi_n = utils.euler_function(n)
+        n_pk, e_pk, d_sk = get_public_key_and_secret_key()
 
-        # choose a number coprime with the Euler function's value of n
-        e = 79
-
-        print(f'The public key is: ({n}, {e})')    
-        conn.sendall(str(n).encode('utf-8'))
-        conn.sendall(str(e).encode('utf-8'))
-
-        # calculate the secret key
-        _, secret_key, _ = utils.my_gcd(e, phi_n)
+        print(f'The public key is: ({n_pk}, {e_pk})')    
+        conn.sendall(str(n_pk).encode('utf-8'))
+        conn.sendall(str(e_pk).encode('utf-8'))
 
         print('Waiting for messages...')
 
-        # receiving the encoded message blocks 
-        the_message = ''
-        while (c := conn.recv(32).decode('utf-8')) != 'close':
-            print(f"Received block: {c}")
-            c = int(c)
-            m = decode(c, secret_key, n)
-            the_message += str(m)
+        while True:
+            # receiving the encoded message blocks 
+            the_message = ''
+            while not (c := conn.recv(32).decode('utf-8')).strip('0').startswith('close'):
+                print(f"Received block: {c}")
+                m = decode(c, d_sk, n_pk)
+                the_message += str(m)                                
 
-        print(f'The message is: {int(the_message)}')
-
+            if c == 'close all': break
+            else: print(f'The message is: {int(the_message)}')
+            
         time.sleep(1) 
--- sender_RSA.py
+++ sender_RSA.py
@@ -4,6 +4,7 @@ import socket
 import sys
 import utils
 
+
 HOST=sys.argv[1]
 PORT=int(sys.argv[2])
 
@@ -21,39 +22,39 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     print(f'The public key is: {n_pk}, {e_pk}')
 
-    the_message = int(input('Enter an integer as message: '))
+    while True:
+        the_message = input('Enter a number (or type enter to quit): ')
+        if the_message == '': 
+            s.send('close all'.encode('utf-8'))
+            break
 
-    len_msg = len(str(the_message))
-    n_pk_len = len(str(n_pk))
+        len_msg, n_pk_len = len(str(the_message)), len(str(n_pk))
 
-    # choosing and sending blocks length
-    block_len = min(32, n_pk_len - 1)
+        # choosing and sending blocks length
+        block_len = min(32, n_pk_len - 1)
 
-    # creating blocks
-    m_blocks = []
-    if len_msg > n_pk_len:
-        for i in range(0, len_msg, block_len):
-            m_blocks.append(int(str(the_message)[i: i + block_len]))
-    else:
-        m_blocks = [the_message]
+        # creating blocks
+        m_blocks = []
+        if len_msg > n_pk_len:
+            for i in range(0, len_msg, block_len):
+                m_blocks.append(int(str(the_message)[i: i + block_len]))
+        else:
+            m_blocks = [the_message]
 
-    print(f"The message has been divided into blocks: {m_blocks}")
+        print(f"The message has been divided into blocks: {m_blocks}")
 
-    # encoding the message
-    c_blocks = []
-    for M in m_blocks:
-        c_blocks.append(encode(M, n_pk, e_pk))
+        # encoding the message
+        c_blocks = []
+        for M in m_blocks:
+            c_blocks.append(encode(M, n_pk, e_pk))
 
-    print(f"The encoded blocks are: {c_blocks}")
+        print(f"The encoded blocks are: {c_blocks}")
 
-    # sending encoded blocks
-    for c in c_blocks:
-        s.send(('0'*(32-len(str(c))) + str(c)).encode('utf-8'))
-    
-    # sending close connection message
-    s.send('close'.encode('utf-8'))
-    
+        # sending encoded blocks
+        for c in c_blocks:
+            s.send(('0'*(32-len(str(c))) + str(c)).encode('utf-8'))
+        
+        # sending close connection message
+        s.send('close'.encode('utf-8'))
+        
     s.close()
-
-# 6882326879666683
-# 6882326879666683
